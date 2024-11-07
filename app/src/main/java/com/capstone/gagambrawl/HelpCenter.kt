b7f8924624d58aa5457387995c18cc8d73d4ed0f
diamosclint <diamosclint@gmail.com>
2024-11-07 22:55:25 +0800

    Created Help Center Activity

---
+++ app/src/main/java/com/capstone/gagambrawl/HelpCenter.kt
@@ -0,0 +1,22 @@
+package com.capstone.gagambrawl.Profile
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.gagambrawl.R
+
+class HelpCenter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_center)
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+            overridePendingTransition(0, 0) // No animation when going back
+        }
+    }
+}
